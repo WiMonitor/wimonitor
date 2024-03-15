dadb54486f27362306826de91ac29dfa9d6717a8
Georgia Li <jli2456@wisc.edu>
2024-03-15 13:04:44 -0500

    airport version for mac, only use for test

--- non-monitor.py
+++ non-monitor.py
@@ -1,31 +1,37 @@
-from wifi import Cell
 import os
-import pandas
+import pandas as pd
 import time
+import subprocess
 from threading import Thread
 
-networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
+networks = pd.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
 networks.set_index("BSSID", inplace=True)
-    
+
 def print_all():
     while True:
         os.system("clear")
         print(networks)
         time.sleep(0.5)
 
-
+def scan_networks(interface='en0'):
+    airport_path = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+    scan_cmd = f"{airport_path} --scan"
+    result = subprocess.check_output(scan_cmd, shell=True).decode('utf-8')
+    for line in result.split('\n'):
+        parts = line.split()
+        if len(parts) >= 5:
+            ssid = parts[0]
+            bssid = parts[1]
+            channel = parts[2]
+            rssi = parts[3]
+            crypto = parts[-1]
+            networks.loc[bssid] = [ssid, rssi, channel, crypto]
 
 if __name__ == "__main__":
-    # start the thread that prints all the networks
     printer = Thread(target=print_all)
     printer.daemon = True
     printer.start()
+
     while True:
-        cells = Cell.all('wlan0')
-        # if the network with the specified ssid is found, print the details
-        target_ssid = "Konoha"
-        for cell in cells:
-            if cell.ssid == target_ssid:
-                # add it to the dataframe
-                networks.loc[cell.address] = (cell.ssid, cell.signal, cell.channel, cell.encryption_type)
-            
+        scan_networks()
+        time.sleep(5)
