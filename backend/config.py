9d5e052c2a700daaa1f1b46cbc41c3a0f6b240b7
yunhao-jiang <account@yunhao.io>
2024-04-17 16:19:38 -0500

    add dns testing using socket + pythondns

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,5 @@ selected_interface = 'wlan0'
 LEASE_FILE_PATH = '/var/lib/dhcp/dhclient.leases'
 NTP_FILE_PATH = '/etc/ntpsec/ntp.conf' # usually NTP config file is located at /etc/ntp.conf or /etc/ntp/ntp.conf, but my sys is different.
 NTP_OFFSET_THRESHOLD = 0.1
+DNS_FILE_PATH = '/etc/resolv.conf'
+DNS_TIMEOUT = 3
---
+++ backend/dns2.py
@@ -0,0 +1,64 @@
+# this file is named dns2 because python will be confused when import dns.resolver, it will consider it as a loop import.
+import dns.resolver, config, socket
+
+def __get_local_dns_servers__():
+    """Helper function to read the local DNS servers from the /etc/resolv.conf file.
+
+    Returns:
+        dns_servers (list): List of local DNS server found.
+    """
+    dns_servers = []
+    try:
+        with open(config.DNS_FILE_PATH, "r") as file:
+            for line in file:
+                if line.startswith('nameserver'):
+                    dns_servers.append(line.strip().split()[1])
+    except Exception as e:
+        print(f"Error reading DNS servers: {e}")
+    return dns_servers
+
+def test_dns_connectivity(dns_servers, test_domain):
+    """This function tests the reachability and resolution of DNS servers. If the DNS is not reachable, the resolution test will be skipped.
+
+    Args:
+        dns_servers (list): List of DNS servers to test
+        test_domain (str): Domain to resolve using DNS servers
+
+    Returns:
+        results (dict): Result of DNS server connectivity and resolution tests
+    """
+    results = {}
+    for server in dns_servers:
+        # Test DNS server is reachable
+        try:
+            sock = socket.create_connection((server, 53), timeout=config.DNS_TIMEOUT)
+            sock.close()
+            results[server] = {"dns_reachable": True}
+        except Exception as e: # TODO: add to error log
+            results[server] = {"dns_reachable": False}
+            print(f"Failed to connect to {server}: {e}")
+            continue # if not reachable, skip resolution test
+        
+        # Test DNS server resolution
+        resolver = dns.resolver.Resolver()
+        resolver.nameservers = [server]
+        try:
+            answers = resolver.resolve(test_domain)
+            results[server]["resolution_success"] = True
+            results[server]["target_domain"] = test_domain
+            results[server]["resolved_ips"] = [answer.address for answer in answers]
+        except Exception as e: # TODO: add to error log
+            results[server]["resolution_success"] = False
+            print(f"Failed to resolve {test_domain} using {server}: {e}")
+    return results
+
+def test_local_dns_servers(test_domain):
+    """This is an easy-to-use wrapper function to test the local DNS servers.
+
+    Args:
+        test_domain (str): Domain to resolve using DNS servers
+
+    Returns:
+        (dict): See test_dns_connectivity() for details
+    """
+    return test_dns_connectivity(__get_local_dns_servers__(), test_domain)
