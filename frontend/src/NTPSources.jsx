a7fd478dd027b7bff7be7f6da3fef4c4d96fa682
Patron <Patron@Patrons-MacBook-Air.local>
2024-04-18 10:20:17 -0500

    typo fix

--- frontend/src/NTPSources.jsx
+++ frontend/src/NTPSources.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './NTPSources.css';
 
 const NTPSources = () => {
     const [sources, setSources] = useState([]);
